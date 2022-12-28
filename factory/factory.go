f0828334beb6b236ef1abb1cc318b2386daa6dd1
Giridhar Bhujanga <giridharmb@gmail.com>
2022-12-27 21:04:06 -0800

    implemented abstract factory

--- factory/factory.go
+++ factory/factory.go
@@ -10,10 +10,14 @@ type sqliteDB struct {
 	database map[string]string
 }
 
+// -----------------------------------------------------
+
 type IDatabase interface {
 	GetData(string) string
 	PutData(string, string)
 }
+
+// -----------------------------------------------------
 
 func (mdb mongoDB) GetData(query string) string {
 	if _, ok := mdb.database[query]; !ok {
@@ -31,6 +35,8 @@ func (sqlDB sqliteDB) GetData(query string) string {
 	return sqlDB.database[query]
 }
 
+// -----------------------------------------------------
+
 func (mdb mongoDB) PutData(query string, data string) {
 	mdb.database[query] = data
 }
@@ -39,12 +45,108 @@ func (sqlDB sqliteDB) PutData(query string, data string) {
 	sqlDB.database[query] = data
 }
 
-func NewDatabaseFactory(env string) IDatabase {
+// -----------------------------------------------------
+
+func NewDatabaseFactory(env string) interface{} {
 	switch env {
 	case "production":
-		return mongoDB{database: make(map[string]string)}
+		return mongoDB{
+			database: make(map[string]string),
+		}
 	case "development":
-		return sqliteDB{database: make(map[string]string)}
+		return sqliteDB{
+			database: make(map[string]string),
+		}
+	default:
+		return nil
+	}
+}
+
+// -----------------------------------------------------
+
+type file struct {
+	name    string
+	content string
+}
+
+type ntfs struct {
+	files map[string]file
+}
+
+type ext4 struct {
+	files map[string]file
+}
+
+// -----------------------------------------------------
+
+type IFileSystem interface {
+	CreateFile(string)
+	FileFile(string) file
+}
+
+// -----------------------------------------------------
+
+func (n ntfs) CreateFile(s string) {
+	myFile := file{
+		name:    "ntfs file",
+		content: s,
+	}
+	n.files[s] = myFile
+	log.Printf("ntfs...")
+}
+
+func (n ntfs) FileFile(s string) file {
+	if _, ok := n.files[s]; !ok {
+		return file{}
+	}
+	return n.files[s]
+}
+
+// -----------------------------------------------------
+
+func (e ext4) CreateFile(s string) {
+	myFile := file{
+		name:    "ext4 file",
+		content: s,
+	}
+	e.files[s] = myFile
+	log.Printf("ext4...")
+}
+
+func (e ext4) FileFile(s string) file {
+	if _, ok := e.files[s]; !ok {
+		return file{}
+	}
+	return e.files[s]
+}
+
+// -----------------------------------------------------
+
+func NewFilesystemFactory(env string) interface{} {
+	switch env {
+	case "production":
+		return ntfs{
+			files: make(map[string]file),
+		}
+	case "development":
+		return ext4{
+			files: make(map[string]file),
+		}
+	default:
+		return nil
+	}
+}
+
+// -----------------------------------------------------
+
+type Factory func(string) interface{}
+
+func AbstractFactory(fact string) Factory {
+	switch fact {
+	case "database":
+		return NewDatabaseFactory
+	case "filesystem":
+		return NewFilesystemFactory
 	default:
 		return nil
 	}
--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"reflect"
 )
 
+func SetupConstructors(env string) (factory.IDatabase, factory.IFileSystem) {
+	fs := factory.AbstractFactory("filesystem")
+	db := factory.AbstractFactory("database")
+
+	return db(env).(factory.IDatabase), fs(env).(factory.IFileSystem)
+}
+
 func main() {
 	env1 := "production"
 	env2 := "development"
 
-	db1 := factory.NewDatabaseFactory(env1)
-	db2 := factory.NewDatabaseFactory(env2)
+	db1, fs1 := SetupConstructors(env1)
+	db2, fs2 := SetupConstructors(env2)
+
+	// -----------------------------------------------------
 
 	db1.PutData("test1", "value1")
 	log.Printf(db1.GetData("test1"))
@@ -26,17 +35,41 @@ func main() {
 	fmt.Println(reflect.TypeOf(&db1).Elem())
 	fmt.Println(reflect.TypeOf(&db2).Elem())
 
+	// -----------------------------------------------------
+
+	fs1.CreateFile("/tmp/fs1.txt")
+	fmt.Println(fs1.FileFile("/tmp/fs1.txt"))
+
+	fs2.CreateFile("/tmp/fs2.txt")
+	fmt.Println(fs2.FileFile("/tmp/fs2.txt"))
+
+	log.Printf(reflect.TypeOf(fs1).Name())
+	log.Printf(reflect.TypeOf(fs2).Name())
+
+	fmt.Println(reflect.TypeOf(&fs1).Elem())
+	fmt.Println(reflect.TypeOf(&fs2).Elem())
+
+	// -----------------------------------------------------
+
 }
 
 /*
 Output
 
-2022/12/27 16:49:23 MongoDB
-2022/12/27 16:49:23 value1
-2022/12/27 16:49:23 SqliteDB
-2022/12/27 16:49:23 value2
-2022/12/27 16:49:23 mongoDB
-2022/12/27 16:49:23 sqliteDB
+2022/12/27 21:03:21 MongoDB
+2022/12/27 21:03:21 value1
+2022/12/27 21:03:21 SqliteDB
+2022/12/27 21:03:21 value2
+2022/12/27 21:03:21 mongoDB
+2022/12/27 21:03:21 sqliteDB
 factory.IDatabase
 factory.IDatabase
+2022/12/27 21:03:21 ntfs...
+{ntfs file /tmp/fs1.txt}
+2022/12/27 21:03:21 ext4...
+{ext4 file /tmp/fs2.txt}
+2022/12/27 21:03:21 ntfs
+2022/12/27 21:03:21 ext4
+factory.IFileSystem
+factory.IFileSystem
 */
