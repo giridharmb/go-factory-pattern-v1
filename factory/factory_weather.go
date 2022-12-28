ffa1d42308aadd6d9589a67c0e4e4ab48e9046ec
Giridhar Bhujanga <giridharmb@gmail.com>
2022-12-28 13:10:31 -0800

    weather abstract factory

---
+++ factory/factory_weather.go
@@ -0,0 +1,125 @@
+package factory
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	endpoint = "https://api.openweathermap.org/data/2.5"
+	//endpoint                = "https://api.openweathermap.org/data/3.0/onecall"
+	pathFormatWeatherByCity = "/weather?q=%s&appid=%s&units=metric"
+)
+
+// -----------------------------------------------------
+
+type WeatherQuery struct {
+	ApiKey string
+	City   string
+}
+
+type Weather struct {
+	Temp     float32
+	Pressure float32
+	MinTemp  float32
+	MaxTemp  float32
+}
+
+type WeatherDataHTTP struct {
+	APIKey string `json:"apikey"`
+	City   string `json:"city"`
+}
+
+type WeatherResponse struct {
+	Message string
+	Main    struct {
+		Temp     float32 `json:"temp"`
+		Pressure float32 `json:"pressure"`
+		TempMin  float32 `json:"temp_min"`
+		TempMax  float32 `json:"temp_max"`
+	}
+}
+
+// -----------------------------------------------------
+
+func (r WeatherResponse) ToWeather() Weather {
+	return Weather{
+		Temp:     r.Main.Temp,
+		Pressure: r.Main.Pressure,
+		MinTemp:  r.Main.TempMin,
+		MaxTemp:  r.Main.TempMax,
+	}
+}
+
+// -----------------------------------------------------
+
+type IWeather interface {
+	GetWeather() (Weather, error)
+}
+
+// -----------------------------------------------------
+
+func (wq WeatherQuery) GetWeather() (Weather, error) {
+	path := fmt.Sprintf(pathFormatWeatherByCity, wq.City, wq.ApiKey)
+	log.Printf("@ provider : path : %v", path)
+
+	completeURL := endpoint + path
+	log.Printf("@ provider : completeURL : %v", completeURL)
+
+	res, err := http.Get(completeURL)
+	if err != nil {
+		return Weather{}, fmt.Errorf("GetWeather() : failed http GET: %s", err)
+	}
+
+	defer func() {
+		_ = res.Body.Close()
+	}()
+
+	// read the response body and encode it into the respose struct
+	bodyRaw, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return Weather{}, fmt.Errorf("GetWeather() : failed reading body: %s", err)
+	}
+
+	var weatherResponse WeatherResponse
+	if err = json.Unmarshal(bodyRaw, &weatherResponse); err != nil {
+		return Weather{}, fmt.Errorf("GetWeather() : failed encoding body: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return Weather{}, fmt.Errorf("GetWeather() : got error from OpenWeather: %s", weatherResponse.Message)
+	}
+
+	// return the external response converted into an entity
+	return weatherResponse.ToWeather(), nil
+}
+
+// -----------------------------------------------------
+
+func NewWeatherFactory(apiKey string, city string) interface{} {
+	weather := WeatherQuery{
+		ApiKey: apiKey,
+		City:   city,
+	}
+	return weather
+}
+
+// -----------------------------------------------------
+
+// Generic Factory
+
+type GFactory func(string, string) interface{}
+
+func GAbstractFactory(fact string) GFactory {
+	switch fact {
+	case "weather":
+		return NewWeatherFactory
+	default:
+		return nil
+	}
+}
+
+// -----------------------------------------------------
--- main.go
+++ main.go
@@ -1,75 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-factory-pattern/factory"
 	"log"
-	"reflect"
 )
 
-func SetupConstructors(env string) (factory.IDatabase, factory.IFileSystem) {
-	fs := factory.AbstractFactory("filesystem")
-	db := factory.AbstractFactory("database")
-
-	return db(env).(factory.IDatabase), fs(env).(factory.IFileSystem)
+func SetupConstructors(apiKey string, city string) factory.IWeather {
+	wf := factory.GAbstractFactory("weather")
+	return wf(apiKey, city).(factory.IWeather)
 }
 
 func main() {
-	env1 := "production"
-	env2 := "development"
 
-	db1, fs1 := SetupConstructors(env1)
-	db2, fs2 := SetupConstructors(env2)
+	weatherI := SetupConstructors("606c01e9eafe84058752ab69f86d74f8", "london")
 
-	// -----------------------------------------------------
+	weather, err := weatherI.GetWeather()
+	if err != nil {
+		log.Printf("%v", err.Error())
+		return
+	}
 
-	db1.PutData("test1", "value1")
-	log.Printf(db1.GetData("test1"))
+	PrettyPrintData(weather)
 
-	db2.PutData("test2", "value2")
-	log.Printf(db2.GetData("test2"))
+}
 
-	log.Printf(reflect.TypeOf(db1).Name())
-	log.Printf(reflect.TypeOf(db2).Name())
-
-	fmt.Println(reflect.TypeOf(&db1).Elem())
-	fmt.Println(reflect.TypeOf(&db2).Elem())
-
-	// -----------------------------------------------------
-
-	fs1.CreateFile("/tmp/fs1.txt")
-	fmt.Println(fs1.FileFile("/tmp/fs1.txt"))
-
-	fs2.CreateFile("/tmp/fs2.txt")
-	fmt.Println(fs2.FileFile("/tmp/fs2.txt"))
-
-	log.Printf(reflect.TypeOf(fs1).Name())
-	log.Printf(reflect.TypeOf(fs2).Name())
-
-	fmt.Println(reflect.TypeOf(&fs1).Elem())
-	fmt.Println(reflect.TypeOf(&fs2).Elem())
-
-	// -----------------------------------------------------
-
+func PrettyPrintData(data interface{}) {
+	dataBytes, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		log.Printf("error : could not MarshalIndent json : %v", err.Error())
+		return
+	}
+	fmt.Printf("\n%v\n\n", string(dataBytes))
 }
 
 /*
 Output
 
-2022/12/27 21:03:21 MongoDB
-2022/12/27 21:03:21 value1
-2022/12/27 21:03:21 SqliteDB
-2022/12/27 21:03:21 value2
-2022/12/27 21:03:21 mongoDB
-2022/12/27 21:03:21 sqliteDB
-factory.IDatabase
-factory.IDatabase
-2022/12/27 21:03:21 ntfs...
-{ntfs file /tmp/fs1.txt}
-2022/12/27 21:03:21 ext4...
-{ext4 file /tmp/fs2.txt}
-2022/12/27 21:03:21 ntfs
-2022/12/27 21:03:21 ext4
-factory.IFileSystem
-factory.IFileSystem
+2022/12/28 13:08:28 @ provider : path : /weather?q=london&appid=606c01e9eafe84058752ab69f86d74f8&units=metric
+2022/12/28 13:08:28 @ provider : completeURL : https://api.openweathermap.org/data/2.5/weather?q=london&appid=606c01e9eafe84058752ab69f86d74f8&units=metric
+
+{
+    "Temp": 10.22,
+    "Pressure": 995,
+    "MinTemp": 9.51,
+    "MaxTemp": 10.99
+}
 */
